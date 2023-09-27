cefd4f92c80bd541cc4c647a31c1e5d246759a92
Fino Liu <liujinfeng@shoplazza.com>
2023-09-27 13:38:12 +0800

    update

--- bash/ip.sh
+++ bash/ip.sh
@@ -3,15 +3,15 @@
 IP=$1
 
 if [[ -z $IP ]]; then
-    lg WARN "======================================="
-    lg INFO "MyIP from myip.ipip.net:"
+    echo "======================================="
+    echo "MyIP from myip.ipip.net:"
     curl -s myip.ipip.net/json | jq
 
-    lg WARN "======================================="
-    lg INFO "MyIP from ipinfo.io:"
+    echo "======================================="
+    echo "MyIP from ipinfo.io:"
     curl -s ipinfo.io/$1?token=1cc7975ae289bb | jq
 else
-    lg WARN "======================================="
-    lg INFO "Custom IP:$IP from ipinfo.io:"
+    echo "======================================="
+    echo "Custom IP:$IP from ipinfo.io:"
     curl -s ipinfo.io/$1?token=1cc7975ae289bb | jq
 fi
