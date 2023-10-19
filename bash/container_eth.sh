29e2a4a887cd612922164ae94d3ef229c8bb68b7
Fino Liu <liujinfeng@shoplazza.com>
2023-10-19 11:57:51 +0800

    feat: add container_eth.sh

--- bash/container_eth.sh
+++ bash/container_eth.sh
@@ -3,6 +3,7 @@
 pod=$1
 
 idx=$(kubectl exec $pod -- cat /sys/class/net/eth0/iflink)
+VETH=""
 
 ls /sys/class/net | grep veth | while read veth; do [[ "$(cat /sys/class/net/$veth/ifindex)" = "$idx" ]] && VETH=$veth; done;
 
